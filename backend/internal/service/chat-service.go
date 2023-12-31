df62b7ead2e592ac167ccc2a9cbe950d8dd838e4
Alex Souphanthong <alexsoup8@gmail.com>
2023-12-31 14:58:49 -0500

    updated chat service

--- backend/internal/service/chat-service.go
+++ backend/internal/service/chat-service.go
@@ -1,27 +1,58 @@
 package service
 
-import "github.com/gorilla/websocket"
+import (
+	"github.com/Alexsoup97/message-app/models"
+	"github.com/gorilla/websocket"
+)
 
 type ChatClient struct {
 	Hub  *ChatHandler
 	Conn *websocket.Conn
-  User string
+	User string
 }
 
 type ChatHandler struct {
-	SendMessage chan []byte
+	SendMessage chan *models.Message
 	Register    chan *ChatClient
 	Unregister  chan *ChatClient
 	Clients     map[string]*ChatClient
+	LiveChats   map[string][]*ChatClient
 }
 
 func (c ChatHandler) init() *ChatHandler {
 	return &ChatHandler{
-		SendMessage: make(chan []byte),
+		SendMessage: make(chan *models.Message),
 		Register:    make(chan *ChatClient),
 		Unregister:  make(chan *ChatClient),
 		Clients:     make(map[string]*ChatClient),
+		LiveChats:   make(map[string][]*ChatClient),
 	}
+
+}
+
+func (client ChatClient) read() {
+
+	defer func() {
+		client.Hub.Unregister <- &client
+		client.Conn.Close()
+	}()
+
+	for {
+		message := new(models.Message)
+		err := client.Conn.ReadJSON(message)
+
+		if err != nil {
+			break
+		}
+
+		client.Hub.SendMessage <- message
+	}
+}
+
+func (client ChatClient) write() {
+	defer func() {
+
+	}()
 
 }
 
@@ -29,12 +60,22 @@ func (c ChatHandler) run() {
 	for {
 		select {
 		case client := <-c.Register:
-      c.Clients[client.]
-
+			c.Clients[client.User] = client
 
 		case client := <-c.Unregister:
+			delete(c.Clients, client.User)
 
 		case message := <-c.SendMessage:
+
+			targets, ok := c.LiveChats[message.ConverstationId]
+
+			if !ok {
+
+			}
+
+			for _, s := range targets {
+
+			}
 
 		}
 	}
